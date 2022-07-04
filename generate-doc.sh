3c4501f390e9785c7ef3b6fd67625d220908a1a8
José Fonseca <jfonseca@aneo.fr>
2022-07-04 23:29:33 +0200

    make the copy localy

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,4 +1,3 @@
 docker run --rm \
-	-v $(pwd)/doc:/out -v \
-	$(pwd)/Protos/V1:/protos pseudomuto/protoc-gen-doc \
-	--doc_opt=markdown,protobuf-generated-documentation.md	
+	-v $(pwd)/Documentation/api:/out -v \
+	$(pwd)/Protos/V1:/protos pseudomuto/protoc-gen-doc
