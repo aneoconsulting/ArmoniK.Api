e00f3606173bffedf0ea31e6391f87be90bfe63d
José Fonseca <jfonseca@aneo.fr>
2022-07-05 16:02:55 +0200

    correct local cpy and script to md

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,3 +1,4 @@
 docker run --rm \
 	-v $(pwd)/Documentation/api:/out -v \
-	$(pwd)/Protos/V1:/protos pseudomuto/protoc-gen-doc
+	$(pwd)/Protos/V1:/protos pseudomuto/protoc-gen-doc \
+	--doc_opt=markdown,index.md
