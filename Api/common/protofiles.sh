9c200ec1d3d8bcef9694b7400c3ea4f3e5612c7b
Dylan Brasseur <dbrasseur@aneo.fr>
2022-10-06 11:21:51 +0200

    Updated python package

---
+++ Api/common/protofiles.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export REPOSITORY_PATH=$(pwd)/../..
+export PROTO_PATH=$REPOSITORY_PATH/Protos/V1
+export README_PATH=$REPOSITORY_PATH/README.md
+
+armonik_worker_files=("agent_service.proto" "worker_service.proto")
+armonik_client_files=("submitter_service.proto" "tasks_service.proto" "sessions_service.proto" \
+                      "results_service.proto" "applications_service.proto")
+armonik_common_files=("objects.proto" "task_status.proto" "session_status.proto" \
+                      "result_status.proto" "agent_common.proto" "sessions_common.proto"  \
+                      "submitter_common.proto"  "tasks_common.proto"  "worker_common.proto" \
+                      "results_common.proto" "applications_common.proto")
