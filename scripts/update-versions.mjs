cb81b843df410e8315a8dbde9acf3ed80653ff0a
esoubiran-aneo <esoubiran@aneo.fr>
2023-01-17 16:22:47 +0100

    feat: create script to verify each version

--- scripts/update-versions.mjs
+++ scripts/update-versions.mjs
@@ -1,6 +1,13 @@
-import fs from "node:fs";
 import consola from "consola";
-import { resolve } from "pathe";
+import { _readAndReplace } from "./versions/_readAndReplace.mjs";
+import {
+  csharpFiles,
+  csharpPattern,
+  jsFiles,
+  jsPattern,
+  pythonFiles,
+  pythonPattern,
+} from "./versions/_contants.mjs";
 
 const [, , ...args] = process.argv;
 
@@ -11,40 +18,13 @@ if (args.length === 0) {
 
 const version = args[0];
 
-const csharpPattern = /<PackageVersion>(.*)<\/PackageVersion>/g;
-const csharpFiles = [
-  "packages/csharp/ArmoniK.Api.Common.Channel/ArmoniK.Api.Common.Channel.csproj",
-  "packages/csharp/ArmoniK.Api.Client/ArmoniK.Api.Client.csproj",
-  "packages/csharp/ArmoniK.Api.Common/ArmoniK.Api.Common.csproj",
-  "packages/csharp/ArmoniK.Api.Core/ArmoniK.Api.Core.csproj",
-  "packages/csharp/ArmoniK.Api.Worker/ArmoniK.Api.Worker.csproj",
-];
-
 consola.info("Updating C# projects to version", version);
 csharpFiles.forEach(
   _readAndReplace(csharpPattern, `<PackageVersion>${version}</PackageVersion>`)
 );
 
-const pythonPattern = /version = "(.*)"/g;
-const pythonFiles = ["packages/python/pyproject.toml"];
-
 consola.info("Updating Python projects to version", version);
 pythonFiles.forEach(_readAndReplace(pythonPattern, `version = "${version}"`));
 
-const jsPattern = /"version": "(.*)"/g;
-const jsFiles = ["package.json"];
-
 consola.info("Updating JS projects to version", version);
 jsFiles.forEach(_readAndReplace(jsPattern, `"version": "${version}"`));
-
-function _readAndReplace(pattern, replace) {
-  return (file) => {
-    const data = fs.readFileSync(resolve(file), "utf8");
-
-    const result = data.replace(pattern, replace);
-
-    fs.writeFileSync(resolve(file), result, "utf8");
-
-    consola.success(`Update ${file.split("/").pop()}`);
-  };
-}
