0605af80292a5160f2dec986e6f4f14e1b158a55
esoubiran-aneo <esoubiran@aneo.fr>
2023-01-30 10:00:13 +0100

    fix: settings

--- .docs/app.config.ts
+++ .docs/app.config.ts
@@ -2,5 +2,13 @@ export default defineAppConfig({
   docus: {
     title: "ArmoniK.Api",
     description: "API for ArmoniK",
+    socials: {
+      github: 'aneoconsulting/ArmoniK.Api',
+    },
+    github: {
+      dir: '.docs/content',
+      repo: 'ArmoniK.Api',
+      owner: 'aneoconsulting',
+    },
   }
 });
