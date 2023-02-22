2a3bf08a2d8afef285814e96e70dc916947c96a0
esoubiran-aneo <esoubiran@aneo.fr>
2023-02-22 11:45:18 +0100

    chore: verify versions

--- scripts/versions/_readAndFind.ts
+++ scripts/versions/_readAndFind.ts
@@ -1,12 +1,16 @@
 import { resolve } from "pathe";
 import consola from "consola";
-import { promises as fsp } from "node:fs";
+import fs from "node:fs";
 
 export function _readAndFind(pattern: RegExp, versions: Map<string, string>) {
-  return async (file: string) => {
-    const data = await fsp.readFile(resolve(file), "utf8");
+  return (file: string) => {
+    const data = fs.readFileSync(resolve(file), {
+      encoding: "utf8",
+      flag: "r",
+    });
 
     const version = pattern.exec(data)?.groups?.version;
+    console.log(version);
 
     if (!version) {
       consola.fatal(`Could not find version in ${file}`);
@@ -14,6 +18,7 @@ export function _readAndFind(pattern: RegExp, versions: Map<string, string>) {
     }
 
     versions.set(file, version);
-    consola.trace(`Found ${file.split("/").pop()}@${version}`);
+    consola.log(`Found ${file.split("/").pop()}@${version}`);
+
   };
 }
