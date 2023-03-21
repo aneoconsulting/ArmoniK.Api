10021477f8a64099dd4f9c233dfdfed39576bb07
esoubiran-aneo <esoubiran@aneo.fr>
2023-03-21 16:43:36 +0100

    docs: update version

--- .docs/nuxt.config.ts
+++ .docs/nuxt.config.ts
@@ -1,7 +1,25 @@
+const baseURL = process.env.NODE_ENV === "production" ? "/ArmoniK.Api/" : "/";
+
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.NODE_ENV === "production" ? "/ArmoniK.Api/" : "",
+    baseURL: baseURL,
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/ico',
+          href: `${baseURL}favicon.ico`,
+        }
+      ]
+    }
   },
 
   extends: "@aneoconsultingfr/armonik-docs-theme",
+
+  runtimeConfig: {
+    public: {
+      siteName: 'ArmoniK.Api',
+      siteDescription: 'API for ArmoniK',
+    }
+  },
 });
