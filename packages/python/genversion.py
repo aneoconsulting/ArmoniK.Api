7ee223abc17b8d445396f79733daf0367577687e
Dylan Brasseur <dbrasseur@aneo.fr>
2023-03-29 18:21:34 +0200

    Updated ci, change version generation

--- packages/python/genversion.py
+++ packages/python/genversion.py
@@ -4,20 +4,36 @@ from argparse import ArgumentParser
 from hashlib import sha256
 
 
-def get_dev_version(version: ScmVersion):
-    return version.format_next_version(guess_next_version, "{guessed}b"+f"{int(sha256(version.branch.encode('ascii')).hexdigest()[:7], 16)}.dev{int(version.node[1:], 16)}")
+class Versionner:
+    def __init__(self, consider_dirty=True):
+        self.consider_dirty = consider_dirty
 
+    def get_dev_version(self, version: ScmVersion):
+        branch_code = f"b{int(sha256(version.branch.encode('ascii')).hexdigest()[:7], 16)}" if version.branch != "main" else ""
+        if version.dirty and self.consider_dirty:
+            return version.format_next_version(guess_next_version,
+                                               "{guessed}" + f"{branch_code}.post{int(version.node[1:], 16)}.dev0")
+        if version.distance == 0:
+            return self.get_rel_version(version)
+        return version.format_next_version(guess_next_version,
+                                           "{guessed}" + f"{branch_code}.post{int(version.node[1:], 16)}")
 
-def get_rel_version(version: ScmVersion):
-    return version.format_next_version(guess_next_version, "{guessed}")
+    def get_rel_version(self, version: ScmVersion):
+        if version.dirty and self.consider_dirty:
+            return version.format_next_version(guess_next_version, "{guessed}.dev0")
+        if version.distance == 0:
+            return version.format_with("{tag}")
+        return version.format_next_version(guess_next_version, "{guessed}")
 
 
 def main():
     parser = ArgumentParser("ArmoniK Python version generator")
     parser.add_argument("-r", "--release", action='store_true')
+    parser.add_argument("-n", "--no-dirty", action='store_true')
     args = parser.parse_args()
     config = Configuration.from_file("./pyproject.toml")
-    config.version_scheme = get_rel_version if args.release else get_dev_version
+    versionner = Versionner(not args.no_dirty)
+    config.version_scheme = versionner.get_rel_version if args.release else versionner.get_dev_version
     version = _get_version(config)
     print(version)
 
