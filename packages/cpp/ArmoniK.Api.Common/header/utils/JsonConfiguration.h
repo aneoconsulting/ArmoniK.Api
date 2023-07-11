c0d3d5af35dabc54544b3c832bf63427c2bc087a
Dylan Brasseur <dbrasseur@aneo.fr>
2023-07-11 18:34:51 +0200

    Format

--- packages/cpp/ArmoniK.Api.Common/header/utils/JsonConfiguration.h
+++ packages/cpp/ArmoniK.Api.Common/header/utils/JsonConfiguration.h
@@ -14,9 +14,8 @@ class JsonConfiguration : public IConfiguration {
 private:
   /**
    * Parsed values
-  */
+   */
   std::unordered_map<std::string, std::string> values;
-
 
   JsonConfiguration() = default;
 
@@ -25,34 +24,28 @@ public:
    * @brief Constructor that takes a JSON file path.
    * @param filepath JSON file path to be used for configuration.
    */
-  explicit JsonConfiguration(const std::string& filepath);
+  explicit JsonConfiguration(const std::string &filepath);
 
-  static JsonConfiguration fromString(const std::string& json_string);
+  static JsonConfiguration fromString(const std::string &json_string);
 
   /**
    * @brief Get the value associated with the given key.
    * @param string Key to look up.
    * @return The value associated with the key, as a string.
    */
-  [[nodiscard]] std::string get(const std::string &string) const override{
-    return values.at(string);
-  }
+  [[nodiscard]] std::string get(const std::string &string) const override { return values.at(string); }
 
   /**
    * @brief Set the value associated with the given key.
    * @param string Key to set the value for.
    * @param value Value to set for the key.
    */
-  void set(const std::string &string, const std::string &value) override{
-    values.insert_or_assign(string, value);
-  }
+  void set(const std::string &string, const std::string &value) override { values.insert_or_assign(string, value); }
 
   /**
    * @brief Set the values from another IConfiguration object.
    * @param other IConfiguration object to copy values from.
    */
-  void set(const IConfiguration &other) override{
-
-  }
+  void set(const IConfiguration &other) override {}
 };
 } // namespace armonik::api::common::utils
