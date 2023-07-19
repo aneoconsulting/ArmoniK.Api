c83fba4b59268daf7b956586d1ce08bc3a674a11
esoubiran-aneo <esoubiran@aneo.fr>
2023-07-19 10:13:55 +0200

    feat: update export from angular package

--- packages/angular/projects/aneoconsultingfr/armonik.api.angular/src/lib/index.ts
+++ packages/angular/projects/aneoconsultingfr/armonik.api.angular/src/lib/index.ts
@@ -5,10 +5,23 @@ export {
   CountTasksByStatusRequest as CountTasksByStatusApplicationRequest,
   CountTasksByStatusResponse as CountTasksByStatusApplicationResponse,
   ApplicationRaw,
-  ApplicationRawEnumField,
-  ApplicationField,
-  ApplicationRawField
 } from "./generated/applications-common.pb";
+export {
+  ApplicationField,
+  ApplicationRawEnumField,
+  ApplicationRawField
+} from "./generated/applications-fields.pb";
+export {
+  FilterArray as ApplicationFilterArray,
+  FilterBoolean as ApplicationFilterBoolean,
+  FilterDate as ApplicationFilterDate,
+  FilterField as ApplicationFilterField,
+  FilterNumber as ApplicationFilterNumber,
+  FilterString as ApplicationFilterString,
+  Filters as ApplicationFilters,
+  FiltersAnd as ApplicationFiltersAnd,
+  FiltersOr as ApplicationFiltersOr
+} from "./generated/applications-filters.pb";
 export { ApplicationsClient } from "./generated/applications-service.pbsc";
 export {
   GetCurrentUserRequest,
@@ -25,10 +38,24 @@ export {
   GetResultRequest,
   GetResultResponse,
   ResultRaw,
-  ResultRawEnumField,
-  ResultField,
-  ResultRawField,
 } from "./generated/results-common.pb";
+export {
+  ResultRawEnumField,
+  ResultRawField,
+  ResultField,
+} from "./generated/results-fields.pb";
+export {
+  FilterArray as ResultFilterArray,
+  FilterBoolean as ResultFilterBoolean,
+  FilterDate as ResultFilterDate,
+  FilterField as ResultFilterField,
+  FilterNumber as ResultFilterNumber,
+  FilterString as ResultFilterString,
+  FilterStatus as ResultFilterStatus,
+  Filters as ResultFilters,
+  FiltersAnd as ResultFiltersAnd,
+  FiltersOr as ResultFiltersOr,
+} from "./generated/results-filters.pb";
 export { ResultsClient } from "./generated/results-service.pbsc";
 export { SessionStatus } from "./generated/session-status.pb";
 export {
@@ -41,13 +68,27 @@ export {
   CountTasksByStatusRequest as CountTasksByStatusSessionRequest,
   CountTasksByStatusResponse as CountTasksByStatusSessionResponse,
   SessionRaw,
+} from "./generated/sessions-common.pb";
+export {
   SessionRawEnumField,
   SessionRawField,
   TaskOptionEnumField as SessionTaskOptionEnumField,
   TaskOptionField as SessionTaskOptionField,
   TaskOptionGenericField as SessionTaskOptionGenericField,
   SessionField
-} from "./generated/sessions-common.pb";
+} from "./generated/sessions-fields.pb";
+export {
+  FilterArray as SessionFilterArray,
+  FilterBoolean as SessionFilterBoolean,
+  FilterDate as SessionFilterDate,
+  FilterField as SessionFilterField,
+  FilterNumber as SessionFilterNumber,
+  FilterString as SessionFilterString,
+  FilterStatus as SessionFilterStatus,
+  Filters as SessionFilters,
+  FiltersAnd as SessionFiltersAnd,
+  FiltersOr as SessionFiltersOr
+} from "./generated/sessions-filters.pb";
 export { SessionsClient } from "./generated/sessions-service.pbsc";
 export { TaskStatus } from "./generated/task-status.pb";
 export {
@@ -79,6 +120,7 @@ export {
   FilterField as TaskFilterField,
   FilterNumber as TaskFilterNumber,
   FilterString as TaskFilterString,
+  FilterStatus as TaskFilterStatus,
   Filters as TaskFilters,
   FiltersAnd as TaskFiltersAnd,
   FiltersOr as TaskFiltersOr,
@@ -96,10 +138,23 @@ export {
   ListPartitionsRequest,
   ListPartitionsResponse,
   PartitionRaw,
+} from "./generated/partitions-common.pb"
+export {
   PartitionRawEnumField,
   PartitionRawField,
   PartitionField,
-} from "./generated/partitions-common.pb"
+} from "./generated/partitions-fields.pb"
+export {
+  FilterArray as PartitionFilterArray,
+  FilterBoolean as PartitionFilterBoolean,
+  FilterDate as PartitionFilterDate,
+  FilterField as PartitionFilterField,
+  FilterNumber as PartitionFilterNumber,
+  FilterString as PartitionFilterString,
+  Filters as PartitionFilters,
+  FiltersAnd as PartitionFiltersAnd,
+  FiltersOr as PartitionFiltersOr,
+} from "./generated/partitions-filters.pb"
 export { PartitionsClient } from "./generated/partitions-service.pbsc"
 export { StatusCount } from "./generated/objects.pb"
 export { ListVersionsRequest, ListVersionsResponse } from "./generated/versions-common.pb"
