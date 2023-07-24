55001d04c47f52d8bf0114ee516f3ef953d71423
esoubiran-aneo <esoubiran@aneo.fr>
2023-07-24 15:37:17 +0200

    feat: export status range from angular package

--- packages/angular/projects/aneoconsultingfr/armonik.api.angular/src/lib/index.ts
+++ packages/angular/projects/aneoconsultingfr/armonik.api.angular/src/lib/index.ts
@@ -1,10 +1,7 @@
-export { SortDirection } from "./generated/sort-direction.pb";
 export {
-  ListApplicationsRequest,
-  ListApplicationsResponse,
-  CountTasksByStatusRequest as CountTasksByStatusApplicationRequest,
-  CountTasksByStatusResponse as CountTasksByStatusApplicationResponse,
-  ApplicationRaw,
+  ApplicationRaw, CountTasksByStatusRequest as CountTasksByStatusApplicationRequest,
+  CountTasksByStatusResponse as CountTasksByStatusApplicationResponse, ListApplicationsRequest,
+  ListApplicationsResponse
 } from "./generated/applications-common.pb";
 export {
   ApplicationField,
@@ -29,121 +26,28 @@ export {
   User
 } from "./generated/auth-common.pb";
 export { AuthenticationClient } from "./generated/auth-service.pbsc";
-export { ResultStatus } from "./generated/result-status.pb";
 export {
-  GetOwnerTaskIdRequest,
-  GetOwnerTaskIdResponse,
-  ListResultsRequest,
-  ListResultsResponse,
-  GetResultRequest,
-  GetResultResponse,
-  ResultRaw,
-} from "./generated/results-common.pb";
-export {
-  ResultRawEnumField,
-  ResultRawField,
-  ResultField,
-} from "./generated/results-fields.pb";
-export {
-  FilterArray as ResultFilterArray,
-  FilterBoolean as ResultFilterBoolean,
-  FilterDate as ResultFilterDate,
-  FilterField as ResultFilterField,
-  FilterNumber as ResultFilterNumber,
-  FilterString as ResultFilterString,
-  FilterStatus as ResultFilterStatus,
-  Filters as ResultFilters,
-  FiltersAnd as ResultFiltersAnd,
-  FiltersOr as ResultFiltersOr,
-} from "./generated/results-filters.pb";
-export { ResultsClient } from "./generated/results-service.pbsc";
-export { SessionStatus } from "./generated/session-status.pb";
-export {
-  CancelSessionRequest,
-  CancelSessionResponse,
-  GetSessionRequest,
-  GetSessionResponse,
-  ListSessionsRequest,
-  ListSessionsResponse,
-  CountTasksByStatusRequest as CountTasksByStatusSessionRequest,
-  CountTasksByStatusResponse as CountTasksByStatusSessionResponse,
-  SessionRaw,
-} from "./generated/sessions-common.pb";
-export {
-  SessionRawEnumField,
-  SessionRawField,
-  TaskOptionEnumField as SessionTaskOptionEnumField,
-  TaskOptionField as SessionTaskOptionField,
-  TaskOptionGenericField as SessionTaskOptionGenericField,
-  SessionField
-} from "./generated/sessions-fields.pb";
-export {
-  FilterArray as SessionFilterArray,
-  FilterBoolean as SessionFilterBoolean,
-  FilterDate as SessionFilterDate,
-  FilterField as SessionFilterField,
-  FilterNumber as SessionFilterNumber,
-  FilterString as SessionFilterString,
-  FilterStatus as SessionFilterStatus,
-  Filters as SessionFilters,
-  FiltersAnd as SessionFiltersAnd,
-  FiltersOr as SessionFiltersOr
-} from "./generated/sessions-filters.pb";
-export { SessionsClient } from "./generated/sessions-service.pbsc";
-export { TaskStatus } from "./generated/task-status.pb";
-export {
-  CancelTasksRequest,
-  CancelTasksResponse,
-  GetResultIdsRequest,
-  GetResultIdsResponse,
-  GetTaskRequest,
-  GetTaskResponse,
-  ListTasksRequest,
-  ListTasksResponse,
-  CountTasksByStatusRequest,
-  CountTasksByStatusResponse,
-  TaskRaw,
-  TaskSummary,
-} from "./generated/tasks-common.pb";
-export {
-  TaskField,
-  TaskOptionEnumField,
-  TaskSummaryEnumField,
-  TaskSummaryField,
-  TaskOptionField,
-  TaskOptionGenericField,
-} from "./generated/tasks-fields.pb";
-export {
-  FilterArray as TaskFilterArray,
-  FilterBoolean as TaskFilterBoolean,
-  FilterDate as TaskFilterDate,
-  FilterField as TaskFilterField,
-  FilterNumber as TaskFilterNumber,
-  FilterString as TaskFilterString,
-  FilterStatus as TaskFilterStatus,
-  Filters as TaskFilters,
-  FiltersAnd as TaskFiltersAnd,
-  FiltersOr as TaskFiltersOr,
-} from "./generated/tasks-filters.pb";
+  EventSubscriptionRequest,
+  EventSubscriptionResponse
+} from './generated/events-common.pb';
+export { EventsClient } from "./generated/events-service.pbsc";
 export {
   FilterArrayRange,
   FilterDateRange,
-  FilterNumberRange,
-  FilterStringRange,
+  FilterNumberRange, FilterStatusRange, FilterStringRange
 } from "./generated/filters-common.pb";
-export { TasksClient } from "./generated/tasks-service.pbsc";
+export { StatusCount, TaskOptions } from "./generated/objects.pb";
 export {
   GetPartitionRequest,
   GetPartitionResponse,
   ListPartitionsRequest,
   ListPartitionsResponse,
-  PartitionRaw,
-} from "./generated/partitions-common.pb"
+  PartitionRaw
+} from "./generated/partitions-common.pb";
 export {
-  PartitionRawEnumField,
-  PartitionRawField,
-  PartitionField,
-} from "./generated/partitions-fields.pb"
+  PartitionField, PartitionRawEnumField,
+  PartitionRawField
+} from "./generated/partitions-fields.pb";
 export {
   FilterArray as PartitionFilterArray,
   FilterBoolean as PartitionFilterBoolean,
@@ -153,15 +57,84 @@ export {
   FilterString as PartitionFilterString,
   Filters as PartitionFilters,
   FiltersAnd as PartitionFiltersAnd,
-  FiltersOr as PartitionFiltersOr,
-} from "./generated/partitions-filters.pb"
-export { PartitionsClient } from "./generated/partitions-service.pbsc"
-export { StatusCount } from "./generated/objects.pb"
-export { ListVersionsRequest, ListVersionsResponse } from "./generated/versions-common.pb"
-export { VersionsClient } from "./generated/versions-service.pbsc"
-export { TaskOptions } from "./generated/objects.pb"
+  FiltersOr as PartitionFiltersOr
+} from "./generated/partitions-filters.pb";
+export { PartitionsClient } from "./generated/partitions-service.pbsc";
+export { ResultStatus } from "./generated/result-status.pb";
 export {
-  EventSubscriptionRequest,
-  EventSubscriptionResponse
- } from './generated/events-common.pb'
-export { EventsClient } from "./generated/events-service.pbsc"
+  GetOwnerTaskIdRequest,
+  GetOwnerTaskIdResponse, GetResultRequest,
+  GetResultResponse, ListResultsRequest,
+  ListResultsResponse, ResultRaw
+} from "./generated/results-common.pb";
+export {
+  ResultField, ResultRawEnumField,
+  ResultRawField
+} from "./generated/results-fields.pb";
+export {
+  FilterArray as ResultFilterArray,
+  FilterBoolean as ResultFilterBoolean,
+  FilterDate as ResultFilterDate,
+  FilterField as ResultFilterField,
+  FilterNumber as ResultFilterNumber, FilterStatus as ResultFilterStatus, FilterString as ResultFilterString, Filters as ResultFilters,
+  FiltersAnd as ResultFiltersAnd,
+  FiltersOr as ResultFiltersOr
+} from "./generated/results-filters.pb";
+export { ResultsClient } from "./generated/results-service.pbsc";
+export { SessionStatus } from "./generated/session-status.pb";
+export {
+  CancelSessionRequest,
+  CancelSessionResponse, CountTasksByStatusRequest as CountTasksByStatusSessionRequest,
+  CountTasksByStatusResponse as CountTasksByStatusSessionResponse, GetSessionRequest,
+  GetSessionResponse,
+  ListSessionsRequest,
+  ListSessionsResponse, SessionRaw
+} from "./generated/sessions-common.pb";
+export {
+  SessionField, SessionRawEnumField,
+  SessionRawField,
+  TaskOptionEnumField as SessionTaskOptionEnumField,
+  TaskOptionField as SessionTaskOptionField,
+  TaskOptionGenericField as SessionTaskOptionGenericField
+} from "./generated/sessions-fields.pb";
+export {
+  FilterArray as SessionFilterArray,
+  FilterBoolean as SessionFilterBoolean,
+  FilterDate as SessionFilterDate,
+  FilterField as SessionFilterField,
+  FilterNumber as SessionFilterNumber, FilterStatus as SessionFilterStatus, FilterString as SessionFilterString, Filters as SessionFilters,
+  FiltersAnd as SessionFiltersAnd,
+  FiltersOr as SessionFiltersOr
+} from "./generated/sessions-filters.pb";
+export { SessionsClient } from "./generated/sessions-service.pbsc";
+export { SortDirection } from "./generated/sort-direction.pb";
+export { TaskStatus } from "./generated/task-status.pb";
+export {
+  CancelTasksRequest,
+  CancelTasksResponse, CountTasksByStatusRequest,
+  CountTasksByStatusResponse, GetResultIdsRequest,
+  GetResultIdsResponse,
+  GetTaskRequest,
+  GetTaskResponse,
+  ListTasksRequest,
+  ListTasksResponse, TaskRaw,
+  TaskSummary
+} from "./generated/tasks-common.pb";
+export {
+  TaskField,
+  TaskOptionEnumField, TaskOptionField,
+  TaskOptionGenericField, TaskSummaryEnumField,
+  TaskSummaryField
+} from "./generated/tasks-fields.pb";
+export {
+  FilterArray as TaskFilterArray,
+  FilterBoolean as TaskFilterBoolean,
+  FilterDate as TaskFilterDate,
+  FilterField as TaskFilterField,
+  FilterNumber as TaskFilterNumber, FilterStatus as TaskFilterStatus, FilterString as TaskFilterString, Filters as TaskFilters,
+  FiltersAnd as TaskFiltersAnd,
+  FiltersOr as TaskFiltersOr
+} from "./generated/tasks-filters.pb";
+export { TasksClient } from "./generated/tasks-service.pbsc";
+export { ListVersionsRequest, ListVersionsResponse } from "./generated/versions-common.pb";
+export { VersionsClient } from "./generated/versions-service.pbsc";
