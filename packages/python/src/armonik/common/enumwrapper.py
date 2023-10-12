3b791585cdd67e64790c2189ac015567377274ce
jeremyzyngeraneo <jzynger@aneo.fr>
2023-10-12 04:07:17 +0200

    refactor: Edit enumwrapper to add CONST for session status names

--- packages/python/src/armonik/common/enumwrapper.py
+++ packages/python/src/armonik/common/enumwrapper.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from ..protogen.common.task_status_pb2 import TaskStatus as RawStatus, _TASKSTATUS, TASK_STATUS_CANCELLED, TASK_STATUS_CANCELLING, TASK_STATUS_COMPLETED, TASK_STATUS_CREATING, TASK_STATUS_DISPATCHED, TASK_STATUS_ERROR, TASK_STATUS_PROCESSED, TASK_STATUS_PROCESSING, TASK_STATUS_SUBMITTED, TASK_STATUS_TIMEOUT, TASK_STATUS_UNSPECIFIED, TASK_STATUS_RETRIED
+from ..protogen.common.session_status_pb2 import SessionStatus as RawSessionStatus, _SESSIONSTATUS, SESSION_STATUS_UNSPECIFIED, SESSION_STATUS_CANCELLED, SESSION_STATUS_RUNNING
 from ..protogen.common.worker_common_pb2 import HealthCheckReply
 from ..protogen.common.sort_direction_pb2 import SORT_DIRECTION_ASC, SORT_DIRECTION_DESC
 
@@ -35,3 +36,13 @@ class TaskStatus:
 class Direction:
     ASC = SORT_DIRECTION_ASC
     DESC = SORT_DIRECTION_DESC
+
+
+class SessionStatus:
+    @staticmethod
+    def name_from_value(status: RawSessionStatus) -> str:
+        return _SESSIONSTATUS.values_by_number[status].name
+
+    UNSPECIFIED = SESSION_STATUS_UNSPECIFIED
+    RUNNING = SESSION_STATUS_RUNNING
+    CANCELLED = SESSION_STATUS_CANCELLED
