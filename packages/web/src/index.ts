d534f2595746ba4d8dd639b752fdf2cd99e42c9b
Faust1 <fdewas@aneo.fr>
2024-02-19 10:10:04 +0100

    fix: missing web and angular index

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,1 +1,177 @@
 export default 'ArmoniK.Api'
+
+export {
+  ApplicationRaw, ListApplicationsRequest,
+  ListApplicationsResponse,
+  ListApplicationsRequest_Sort,
+} from './generated/applications_common'
+export {
+  ApplicationField,
+  ApplicationRawEnumField,
+  ApplicationRawField,
+  applicationRawEnumFieldFromJSON, applicationRawEnumFieldToJSON
+} from './generated/applications_fields'
+export {
+  FilterField as ApplicationFilterField,
+  Filters as ApplicationFilters,
+  FiltersAnd as ApplicationFiltersAnd,
+} from './generated/applications_filters'
+export { ApplicationsClientImpl as ApplicationsClient, ApplicationsServiceName, } from './generated/applications_service'
+export {
+  GetCurrentUserRequest,
+  GetCurrentUserResponse,
+  User,
+} from './generated/auth_common'
+export { AuthenticationClientImpl as AuthenticationClient, AuthenticationServiceName, } from './generated/auth_service'
+export {
+  EventSubscriptionRequest,
+  EventSubscriptionResponse,
+  EventsEnum,
+  EventSubscriptionResponse_NewResult,
+  EventSubscriptionResponse_NewTask,
+  EventSubscriptionResponse_ResultOwnerUpdate,
+  EventSubscriptionResponse_ResultStatusUpdate,
+  EventSubscriptionResponse_TaskStatusUpdate,
+} from './generated/events_common'
+export { EventsClientImpl as EventsClient, EventsServiceName } from './generated/events_service'
+export {
+  FilterArrayOperator,
+  FilterDateOperator,
+  FilterNumberOperator, FilterStatusOperator, FilterStringOperator,
+  FilterArray,
+  FilterBoolean,
+  FilterDate,
+  FilterNumber,
+  FilterString,
+  FilterBooleanOperator,
+  FilterDuration,
+  FilterDurationOperator,
+  filterArrayOperatorFromJSON, filterArrayOperatorToJSON,
+  filterBooleanOperatorFromJSON, filterBooleanOperatorToJSON,
+  filterDateOperatorFromJSON, filterDateOperatorToJSON,
+  filterDurationOperatorFromJSON, filterDurationOperatorToJSON,
+  filterNumberOperatorFromJSON, filterNumberOperatorToJSON,
+  filterStatusOperatorFromJSON, filterStatusOperatorToJSON,
+  filterStringOperatorFromJSON, filterStringOperatorToJSON
+} from './generated/filters_common'
+export { StatusCount, TaskOptions } from './generated/objects'
+export {
+  GetPartitionRequest,
+  GetPartitionResponse,
+  ListPartitionsRequest,
+  ListPartitionsResponse,
+  ListPartitionsRequest_Sort,
+  PartitionRaw,
+  PartitionRaw_PodConfigurationEntry,
+} from './generated/partitions_common'
+export {
+  PartitionField, PartitionRawEnumField,
+  PartitionRawField,
+  partitionRawEnumFieldFromJSON, partitionRawEnumFieldToJSON
+} from './generated/partitions_fields'
+export {
+  FilterField as PartitionFilterField,
+  Filters as PartitionFilters,
+  FiltersAnd as PartitionFiltersAnd,
+} from './generated/partitions_filters'
+export { PartitionsClientImpl as PartitionsClient, PartitionsServiceName } from './generated/partitions_service'
+export { ResultStatus, resultStatusFromJSON, resultStatusToJSON } from './generated/result_status'
+export {
+  GetOwnerTaskIdRequest,
+  CreateResultsMetaDataRequest, CreateResultsMetaDataResponse,
+  CreateResultsRequest, CreateResultsResponse,
+  DeleteResultsDataRequest, DeleteResultsDataResponse,
+  DownloadResultDataRequest, DownloadResultDataResponse,
+  ResultsServiceConfigurationResponse, UploadResultDataRequest,
+  UploadResultDataResponse, WatchResultRequest,
+  WatchResultResponse,
+  GetOwnerTaskIdResponse, GetResultRequest,
+  GetResultResponse, ListResultsRequest,
+  ListResultsResponse, ResultRaw,
+  CreateResultsMetaDataRequest_ResultCreate, CreateResultsRequest_ResultCreate,
+  GetOwnerTaskIdResponse_MapResultTask, ListResultsRequest_Sort,
+  UploadResultDataRequest_ResultIdentifier,
+} from './generated/results_common'
+export {
+  ResultField, ResultRawEnumField,
+  ResultRawField,
+  resultRawEnumFieldFromJSON, resultRawEnumFieldToJSON
+} from './generated/results_fields'
+export {
+  FilterField as ResultFilterField,
+  Filters as ResultFilters,
+  FiltersAnd as ResultFiltersAnd,
+  FilterStatus as ResultFilterStatus,
+} from './generated/results_filters'
+export { ResultsClientImpl as ResultsClient, ResultsServiceName } from './generated/results_service'
+export { SessionStatus, sessionStatusFromJSON, sessionStatusToJSON } from './generated/session_status'
+export {
+  CancelSessionRequest, CancelSessionResponse, 
+  GetSessionRequest, GetSessionResponse,
+  ListSessionsRequest, ListSessionsResponse,
+  PauseSessionRequest, PauseSessionResponse,
+  ResumeSessionRequest, ResumeSessionResponse,
+  CloseSessionRequest, CloseSessionResponse,
+  CreateSessionReply, CreateSessionRequest,
+  DeleteSessionRequest, DeleteSessionResponse,
+  PurgeSessionRequest, PurgeSessionResponse,
+  StopSubmissionRequest, StopSubmissionResponse,
+  ListSessionsRequest_Sort,
+  SessionRaw,
+} from './generated/sessions_common'
+export {
+  SessionField, SessionRawEnumField,
+  SessionRawField,
+  TaskOptionEnumField as SessionTaskOptionEnumField,
+  TaskOptionField as SessionTaskOptionField,
+  TaskOptionGenericField as SessionTaskOptionGenericField,
+  sessionRawEnumFieldFromJSON, sessionRawEnumFieldToJSON,
+  taskOptionEnumFieldFromJSON, taskOptionEnumFieldToJSON
+} from './generated/sessions_fields'
+export {
+  FilterField as SessionFilterField,
+  Filters as SessionFilters,
+  FiltersAnd as SessionFiltersAnd,
+  FilterStatus as SessionFilterStatus,
+} from './generated/sessions_filters'
+export { SessionsClientImpl as SessionsClient, SessionsServiceName } from './generated/sessions_service'
+export { SortDirection, sortDirectionFromJSON, sortDirectionToJSON } from './generated/sort_direction'
+export { TaskStatus, taskStatusFromJSON, taskStatusToJSON } from './generated/task_status'
+export {
+  CancelTasksRequest,
+  CancelTasksResponse, CountTasksByStatusRequest,
+  CountTasksByStatusResponse, GetResultIdsRequest,
+  GetResultIdsResponse,
+  GetTaskRequest,
+  GetTaskResponse,
+  ListTasksRequest,
+  ListTasksResponse, TaskDetailed,
+  TaskSummary,
+  ListTasksDetailedResponse, SubmitTasksRequest, SubmitTasksResponse,
+  GetResultIdsResponse_MapTaskResult, ListTasksRequest_Sort,
+  SubmitTasksRequest_TaskCreation, SubmitTasksResponse_TaskInfo,
+  TaskDetailed_Output,
+} from './generated/tasks_common'
+export {
+  TaskField,
+  TaskOptionEnumField, TaskOptionField,
+  TaskOptionGenericField, TaskSummaryEnumField,
+  TaskSummaryField,
+  taskSummaryEnumFieldFromJSON, taskSummaryEnumFieldToJSON
+} from './generated/tasks_fields'
+export {
+  FilterField as TaskFilterField,
+  Filters as TaskFilters,
+  FiltersAnd as TaskFiltersAnd,
+  FilterStatus as TaskFilterStatus,
+} from './generated/tasks_filters'
+export { TasksClientImpl as TasksClient, TasksServiceName } from './generated/tasks_service'
+export { ListVersionsRequest, ListVersionsResponse } from './generated/versions_common'
+export { VersionsClientImpl as VersionsClient, VersionsServiceName } from './generated/versions_service'
+export {
+  CheckHealthRequest,
+  CheckHealthResponse,
+  HealthStatusEnum,
+  healthStatusEnumFromJSON, healthStatusEnumToJSON
+} from './generated/health_checks_common'
+export { HealthChecksServiceClientImpl as HealthChecksServiceClient, HealthChecksServiceServiceName } from './generated/health_checks_service'
