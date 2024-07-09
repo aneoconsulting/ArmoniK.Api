69496a9d24b66effc62af6e548c05ca07134d7ec
qdelamea <qdelamea@aneo.fr>
2024-07-09 17:39:13 +0200

    fix: Python API event client wait_for_result_availability method

--- packages/python/src/armonik/client/events.py
+++ packages/python/src/armonik/client/events.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import Callable, cast, Iterable, List, Optional
 
 from grpc import Channel
@@ -56,66 +57,84 @@ class ArmoniKEvents:
             event_types: The list of the types of event to catch.
             event_handlers: The list of handlers that process the events. Handlers are evaluated in he order they are provided.
                 An handler takes three positional arguments: the ID of the session, the type of event and the event as an object.
-                An handler returns a boolean, if True the process continues, otherwise the stream is closed and the service stops
-                listening to new events.
+                An handler returns a boolean, if False the process continues, otherwise the stream is closed and the service stops
+                listening to new events. As handlers are evaluated in order, when a handler interrupts execution (by returning True)
+                all the handlers following it will not be executed.
             task_filter: A filter on tasks.
             result_filter: A filter on results.
 
         """
-        request = EventSubscriptionRequest(session_id=session_id, returned_events=event_types)
-        if task_filter:
-            request.tasks_filters = cast(rawTaskFilters, task_filter.to_disjunction().to_message())
-        if result_filter:
-            request.results_filters = cast(
+        request = EventSubscriptionRequest(
+            session_id=session_id,
+            returned_events=event_types,
+            tasks_filters=cast(rawTaskFilters, task_filter.to_disjunction().to_message()) if task_filter else rawTaskFilters(),
+            results_filters=cast(
                 rawResultFilters, result_filter.to_disjunction().to_message()
-            )
+            ) if result_filter else rawResultFilters(),
+        )
 
         streaming_call = self._client.GetEvents(request)
         for message in streaming_call:
             event_type = message.WhichOneof("update")
-            if any(
-                [
-                    event_handler(
-                        session_id,
-                        EventTypes.from_string(event_type),
-                        self._events_obj_mapping[event_type].from_raw_event(
-                            getattr(message, event_type)
-                        ),
-                    )
-                    for event_handler in event_handlers
-                ]
-            ):
-                break
+            for event_handler in event_handlers: 
+                if event_handler(
+                    session_id,
+                    EventTypes.from_string(event_type),
+                    self._events_obj_mapping[event_type].from_raw_event(
+                        getattr(message, event_type)
+                    ),
+                ):
+                    break
 
-    def wait_for_result_availability(self, result_id: str, session_id: str) -> None:
+    def wait_for_result_availability(self, result_ids: List[str], session_id: str) -> None:
         """Wait until a result is ready i.e its status updates to COMPLETED.
 
         Args:
-            result_id: The ID of the result.
+            result_ids: The IDs of the results.
             session_id: The ID of the session.
 
         Raises:
             RuntimeError: If the result status is ABORTED.
         """
 
-        def handler(session_id, event_type, event):
-            if not isinstance(event, ResultStatusUpdateEvent):
-                raise ValueError("Handler should receive event of type 'ResultStatusUpdateEvent'.")
-            if event.status == ResultStatus.COMPLETED:
-                return False
-            elif event.status == ResultStatus.ABORTED:
-                raise RuntimeError(f"Result {result.name} with ID {result_id} is aborted.")
-            return True
+        results_not_found = deepcopy(result_ids)
 
-        result = self._results_client.get_result(result_id)
-        if result.status == ResultStatus.COMPLETED:
-            return
-        elif result.status == ResultStatus.ABORTED:
-            raise RuntimeError(f"Result {result.name} with ID {result_id} is aborted.")
+        results_filter = (ResultFieldFilter.RESULT_ID == result_ids[0])
+        for result_id in result_ids[1:]:
+            results_filter = results_filter | (ResultFieldFilter.RESULT_ID == result_id)
 
-        self.get_events(
-            session_id,
-            [EventTypes.RESULT_STATUS_UPDATE],
-            [handler],
-            result_filter=(ResultFieldFilter.RESULT_ID == result_id),
+        request = EventSubscriptionRequest(
+            session_id=session_id,
+            returned_events=[EventTypes.RESULT_STATUS_UPDATE],
+            tasks_filters=rawTaskFilters(),
+            results_filters=cast(
+                rawResultFilters, results_filter.to_disjunction().to_message()
+            ),
         )
+
+        while results_not_found:
+            streaming_call = self._client.GetEvents(request)
+            try:
+                for message in streaming_call:
+                    event_type = EventTypes.from_string(message.WhichOneof("update"))
+                    if event_type == EventTypes.RESULT_STATUS_UPDATE:
+                        event = ResultStatusUpdateEvent.from_raw_event(message.result_status_update)
+                        if event.result_id in results_not_found:
+                            if event.status == ResultStatus.COMPLETED:
+                                results_not_found.remove(event.result_id)
+                                if not results_not_found:
+                                    break
+                            if event.status == ResultStatus.ABORTED:
+                                raise RuntimeError(f"Result {event.result_id} has been aborted.")
+                    if event_type == EventTypes.NEW_RESULT:
+                        event = NewResultEvent.from_raw_event(messae.new_result)
+                        if event.result_id in results_not_found:
+                            if event.status == ResultStatus.COMPLETED:
+                                results_not_found.remove(event.result_id)
+                                if not results_not_found:
+                                    break
+                            if event.status == ResultStatus.ABORTED:
+                                RuntimeError(f"Result {event.result_id} has been aborted.")
+
+            except grpc.RpcError:
+                pass
