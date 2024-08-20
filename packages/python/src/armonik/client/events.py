07b4e6ed969c87de7e3eec8537e028de9788e079
Dylan Brasseur <dbrasseur@aneo.fr>
2024-08-20 10:57:32 +0200

    Fixed event handling

--- packages/python/src/armonik/client/events.py
+++ packages/python/src/armonik/client/events.py
@@ -1,5 +1,5 @@
-from copy import deepcopy
-from typing import Callable, cast, Iterable, List, Optional
+from __future__ import annotations
+from typing import Callable, cast, Iterable, List, Optional, Union
 
 from grpc import Channel, RpcError
 
@@ -13,11 +13,11 @@ from ..common import (
     TaskStatusUpdateEvent,
     ResultStatus,
     Event,
+    Result,
 )
-from .results import ResultFieldFilter
 from ..common.filter import Filter
 from ..protogen.client.events_service_pb2_grpc import EventsStub
-from ..protogen.common.events_common_pb2 import EventSubscriptionRequest
+from ..protogen.common.events_common_pb2 import EventSubscriptionRequest, EventsEnum
 from ..protogen.common.results_filters_pb2 import Filters as rawResultFilters
 from ..protogen.common.tasks_filters_pb2 import Filters as rawTaskFilters
 
@@ -44,7 +44,7 @@ class ArmoniKEvents:
         self,
         session_id: str,
         event_types: Iterable[
-            EventTypes
+            EventsEnum
         ],  # TODO: make EventTypes an enum when Python 3.8 support will be not supported
         event_handlers: List[Callable[[str, EventTypes, Event], bool]],
         task_filter: Optional[Filter] = None,
@@ -55,7 +55,7 @@ class ArmoniKEvents:
         Args:
             session_id: The ID of the session.
             event_types: The list of the types of event to catch.
-            event_handlers: The list of handlers that process the events. Handlers are evaluated in he order they are provided.
+            event_handlers: The list of handlers that process the events. Handlers are evaluated in the order they are provided.
                 An handler takes three positional arguments: the ID of the session, the type of event and the event as an object.
                 An handler returns a boolean, if False the process continues, otherwise the stream is closed and the service stops
                 listening to new events. As handlers are evaluated in order, when a handler interrupts execution (by returning True)
@@ -68,27 +68,31 @@ class ArmoniKEvents:
             session_id=session_id,
             returned_events=event_types,
             tasks_filters=cast(rawTaskFilters, task_filter.to_disjunction().to_message())
-            if task_filter
+            if task_filter is not None
             else rawTaskFilters(),
             results_filters=cast(rawResultFilters, result_filter.to_disjunction().to_message())
-            if result_filter
+            if result_filter is not None
             else rawResultFilters(),
         )
 
         streaming_call = self._client.GetEvents(request)
         for message in streaming_call:
             event_type = message.WhichOneof("update")
-            for event_handler in event_handlers:
-                if event_handler(
+            if any(
+                event_handler(
                     session_id,
                     EventTypes.from_string(event_type),
                     self._events_obj_mapping[event_type].from_raw_event(
                         getattr(message, event_type)
                     ),
-                ):
-                    break
+                )
+                for event_handler in event_handlers
+            ):
+                break
 
-    def wait_for_result_availability(self, result_ids: List[str], session_id: str) -> None:
+    def wait_for_result_availability(
+        self, result_ids: Union[str, List[str]], session_id: str
+    ) -> None:
         """Wait until a result is ready i.e its status updates to COMPLETED.
 
         Args:
@@ -98,43 +102,37 @@ class ArmoniKEvents:
         Raises:
             RuntimeError: If the result status is ABORTED.
         """
+        if isinstance(result_ids, str):
+            result_ids = [result_ids]
+        if len(result_ids) == 0:
+            return
+        results_not_found = set(result_ids)
 
-        results_not_found = deepcopy(result_ids)
-
-        results_filter = ResultFieldFilter.RESULT_ID == result_ids[0]
+        results_filter = Result.result_id == result_ids[0]
         for result_id in result_ids[1:]:
-            results_filter = results_filter | (ResultFieldFilter.RESULT_ID == result_id)
+            results_filter = results_filter | (Result.result_id == result_id)
 
-        request = EventSubscriptionRequest(
-            session_id=session_id,
-            returned_events=[EventTypes.RESULT_STATUS_UPDATE],
-            tasks_filters=rawTaskFilters(),
-            results_filters=cast(rawResultFilters, results_filter.to_disjunction().to_message()),
-        )
+        def handler(_, _2, event: Event) -> bool:
+            event = cast(Union[NewResultEvent, ResultStatusUpdateEvent], event)
+            if event.result_id in results_not_found:
+                if event.status == ResultStatus.COMPLETED:
+                    results_not_found.remove(event.result_id)
+                    if not results_not_found:
+                        return True
+                elif event.status == ResultStatus.ABORTED:
+                    raise RuntimeError(f"Result {event.result_id} has been aborted.")
+            return False
 
         while results_not_found:
-            streaming_call = self._client.GetEvents(request)
             try:
-                for message in streaming_call:
-                    event_type = EventTypes.from_string(message.WhichOneof("update"))
-                    if event_type == EventTypes.RESULT_STATUS_UPDATE:
-                        event = ResultStatusUpdateEvent.from_raw_event(message.result_status_update)
-                        if event.result_id in results_not_found:
-                            if event.status == ResultStatus.COMPLETED:
-                                results_not_found.remove(event.result_id)
-                                if not results_not_found:
-                                    break
-                            if event.status == ResultStatus.ABORTED:
-                                raise RuntimeError(f"Result {event.result_id} has been aborted.")
-                    if event_type == EventTypes.NEW_RESULT:
-                        event = NewResultEvent.from_raw_event(message.new_result)
-                        if event.result_id in results_not_found:
-                            if event.status == ResultStatus.COMPLETED:
-                                results_not_found.remove(event.result_id)
-                                if not results_not_found:
-                                    break
-                            if event.status == ResultStatus.ABORTED:
-                                RuntimeError(f"Result {event.result_id} has been aborted.")
-
+                self.get_events(
+                    session_id,
+                    [EventTypes.RESULT_STATUS_UPDATE, EventTypes.NEW_RESULT],
+                    [handler],
+                    None,
+                    results_filter,
+                )
             except RpcError:
                 pass
+            else:
+                break
