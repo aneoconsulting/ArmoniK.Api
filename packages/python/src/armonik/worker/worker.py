82b75ce62b1df5abc0da84002bf1f3134169f24e
Dylan Brasseur <dbrasseur@aneo.fr>
2024-08-30 15:17:27 +0200

    armonik_worker decorator

--- packages/python/src/armonik/worker/worker.py
+++ packages/python/src/armonik/worker/worker.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
+
+import logging
+import os
 import traceback
 from concurrent import futures
-from typing import Callable, Union
+from logging import Logger
+from typing import Callable, Union, Optional, Tuple
 
 import grpc
 from grpc import Channel
 
+from ..common.channel import create_channel
 from .seqlogger import ClefLogger
 from ..common import Output, HealthCheckStatus
 from ..protogen.common.objects_pb2 import Empty
@@ -74,3 +79,87 @@ class ArmoniKWorker(WorkerServicer):
 
     def HealthCheck(self, request: Empty, context) -> HealthCheckReply:
         return HealthCheckReply(status=self.health_check())
+
+
+def armonik_worker(
+    *,
+    autorun: bool = False,
+    logger: Optional[Logger] = None,
+    worker_endpoint: Optional[str] = None,
+    agent_endpoint: Optional[str] = None,
+    channel_options: Tuple[Tuple[str, str]] = (("grpc.default_authority", "localhost"),),
+):
+    """
+    Transforms the function into an ArmoniK Worker
+    Args:
+        autorun: if True, runs the processor instead of returning the function
+        logger: Logger to use, if None will use the default ClefLogger
+        worker_endpoint: Worker endpoint, if None will use the default from ComputePlane__WorkerChannel__SocketType and ComputePlane__WorkerChannel__Address
+        agent_endpoint: Agent endpoint, if None will use the default from ComputePlane__AgentChannel__SocketType and ComputePlane__AgentChannel__Address
+        channel_options: Options for the gRPC channel
+
+    Returns:
+        Worker function
+    """
+    if logger is None:
+        ClefLogger.setup_logging(logging.INFO)
+        logger = ClefLogger.getLogger("ArmoniKWorker")
+    if worker_endpoint is None:
+        worker_scheme = (
+            "unix://"
+            if os.getenv("ComputePlane__WorkerChannel__SocketType", "unixdomainsocket")
+            == "unixdomainsocket"
+            else "http://"
+        )
+        worker_endpoint = worker_scheme + os.getenv(
+            "ComputePlane__WorkerChannel__Address", "/cache/armonik_worker.sock"
+        )
+    if agent_endpoint is None:
+        agent_scheme = (
+            "unix://"
+            if os.getenv("ComputePlane__AgentChannel__SocketType", "unixdomainsocket")
+            == "unixdomainsocket"
+            else "http://"
+        )
+        agent_endpoint = agent_scheme + os.getenv(
+            "ComputePlane__AgentChannel__Address", "/cache/armonik_agent.sock"
+        )
+
+    def decorator(
+        processor: Callable[[TaskHandler], Output],
+    ) -> Optional[Callable[[Optional[Channel]], None]]:
+        def run(channel: Optional[Channel] = None):
+            """
+            Runs the worker
+            Args:
+                channel: gRPC channel to use, if None creates a channel
+
+            Returns:
+                None
+            """
+            # Start worker
+            logger.info("Worker Started")
+            # Use options to fix Unix socket connection on localhost (cf: <GitHub>)
+            if channel is None:
+                channel_ = create_channel(agent_endpoint, options=channel_options).__enter__()
+            else:
+                channel_ = channel
+            try:
+                worker = ArmoniKWorker(channel_, processor, logger=logger)
+                logger.info("Worker Connected")
+                worker.start(worker_endpoint)
+            except Exception as e:
+                if channel is None:
+                    channel_.__exit__(type(e), e, e.__traceback__)
+                    channel_ = None
+                raise
+            finally:
+                if channel is None and channel_ is not None:
+                    channel_.__exit__(None, None, None)
+
+        if autorun:
+            run()
+
+        return run
+
+    return decorator
