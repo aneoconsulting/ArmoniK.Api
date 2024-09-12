3480a62d34b3476c2cf3b2adf2840a1e5a2de198
Daouda Diakite <ddiakite@aneo.fr>
2024-09-12 16:05:12 +0200

    Fix curl request for all tests unsecure tls mtls

--- packages/cpp/ArmoniK.Api.Tests/source/MockTest.cpp
+++ packages/cpp/ArmoniK.Api.Tests/source/MockTest.cpp
@@ -22,11 +22,19 @@ size_t WriteCallback(void *ptr, size_t size, size_t num_elt, std::string *data) 
 
 bool rpcCalled(absl::string_view service_name, absl::string_view rpc_name, int num_calls, absl::string_view endpoint) {
 
+  armonik::api::common::utils::Configuration config;
+  config.add_json_configuration("appsettings.json").add_env_configuration();
+  std::string call_endpoint = config.get("Http__EndPoint") + "/calls.json";
   auto curl = curl_easy_init();
   std::string read_buffer;
   std::cout << endpoint << std::endl;
+  std::cout << call_endpoint << std::endl;
   if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, endpoint.data());
+    curl_easy_setopt(curl, CURLOPT_URL, call_endpoint.c_str());
+    curl_easy_setopt(curl, CURLOPT_CAINFO, config.get("Grpc__CaCert").c_str());
+    curl_easy_setopt(curl, CURLOPT_SSLCERT, config.get("Grpc__ClientCert").c_str());
+    curl_easy_setopt(curl, CURLOPT_SSLKEY, config.get("Grpc__ClientKey").c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
 
@@ -45,20 +53,28 @@ bool rpcCalled(absl::string_view service_name, absl::string_view rpc_name, int n
     if (response_json[service_name.data()][rpc_name.data()].get_int64() == num_calls) {
       return true;
     }
+    std::cout << "Given number of RPC calls " << num_calls << std::endl;
+    std::cout << "Actual number of RPC calls " << response_json[service_name][rpc_name] << std::endl;
   } catch (const simdjson_error &e) {
     std::cerr << "Failed to parse JSON: " << e.what() << std::endl;
   }
-
   return false;
 }
 
 bool all_rpc_called(absl::string_view service_name, const std::vector<std::string> &missings,
                     absl::string_view endpoint) {
+  armonik::api::common::utils::Configuration config;
+  config.add_json_configuration("appsettings.json").add_env_configuration();
+  std::string call_endpoint = config.get("Http__EndPoint") + "/calls.json";
   auto curl = curl_easy_init();
   std::string read_buffer;
   std::cout << endpoint << std::endl;
   if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, endpoint.data());
+    curl_easy_setopt(curl, CURLOPT_URL, call_endpoint.c_str());
+    curl_easy_setopt(curl, CURLOPT_CAINFO, config.get("Grpc__CaCert").c_str());
+    curl_easy_setopt(curl, CURLOPT_SSLCERT, config.get("Grpc__ClientCert").c_str());
+    curl_easy_setopt(curl, CURLOPT_SSLKEY, config.get("Grpc__ClientKey").c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
 
@@ -74,8 +90,6 @@ bool all_rpc_called(absl::string_view service_name, const std::vector<std::strin
 
   try {
     dom::element response_json = parser.parse(read_buffer);
-
-    dom::array rpcs = response_json[service_name.data()];
 
     std::vector<std::string> missing_rpcs;
     for (auto rpc_name : response_json[service_name.data()].get_array()) {
@@ -100,12 +114,19 @@ bool all_rpc_called(absl::string_view service_name, const std::vector<std::strin
   return true;
 }
 
-void clean_up(absl::string_view endpoint) {
+void clean_up() {
+  armonik::api::common::utils::Configuration config;
+  config.add_json_configuration("appsettings.json").add_env_configuration();
+  std::string reset_endpoint = config.get("Http__EndPoint") + "/reset";
   auto curl = curl_easy_init();
   std::string read_buffer;
   if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, endpoint.data());
+    curl_easy_setopt(curl, CURLOPT_URL, reset_endpoint.c_str());
+    curl_easy_setopt(curl, CURLOPT_CAINFO, config.get("Grpc__CaCert").c_str());
+    curl_easy_setopt(curl, CURLOPT_SSLCERT, config.get("Grpc__ClientCert").c_str());
+    curl_easy_setopt(curl, CURLOPT_SSLKEY, config.get("Grpc__ClientKey").c_str());
     curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
     auto res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
       std::cout << "Request failed: " << curl_easy_strerror(res) << std::endl;
