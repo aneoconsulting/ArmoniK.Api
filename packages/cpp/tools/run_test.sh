c146cd93acd3d11015c5d0d7ca5a93d713797d16
Daouda Diakite <ddiakite@aneo.fr>
2024-10-21 14:57:06 +0200

    Refactor test ci

--- packages/cpp/tools/run_test.sh
+++ packages/cpp/tools/run_test.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
+set -x
 script_path="$(dirname "${BASH_SOURCE:-$0}")"
-working_dir="$(realpath "${script_path}/../../../" )"
-dockerfile="${1:-"${working_dir}/packages/cpp/ArmoniK.Api.Tests/Dockerfile"}"
-image_tag="${2:-"armonik-api-cpp:0.1.0"}"
-docker build --rm -t "$image_tag" -f "$dockerfile" --progress plain "$working_dir"
+CertFolder="$(realpath "$script_path/../../csharp/certs" )"
+docker run --rm -t --network host -v "$CertFolder:/app/source/certs" -v "/usr/local/share/ca-certificates/:/usr/local/share/ca-certificates" \
+            -e Grpc__EndPoint="$Grpc__Endpoint" \
+            -e Http__EndPoint="${Http__Endpoint:-$Grpc__Endpoint}" \
+            ${Grpc__SSLValidation:+-e Grpc__SSLValidation="$Grpc__SSLValidation"} \
+            ${Grpc__CaCert:+-e Grpc__CaCert="/app/source/certs/server1-ca.pem"} \
+            ${Grpc__mTLS:+-e Grpc__mTLS="$Grpc__mTLS"} \
+            ${Grpc__ClientCert:+-e Grpc__ClientCert="/app/source/certs/client.pem"} \
+            ${Grpc__ClientKey:+-e Grpc__ClientKey="/app/source/certs/client.key"} \
+            "armonik-api-cpp:0.1.0"
---
+++ scripts/mock_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <test_dir> <test_command>"
+  exit 1
+fi
+
+script_path="$(dirname "${BASH_SOURCE:-$0}")"
+working_dir="$(realpath "$script_path/../packages" )"
+
+TEST_DIR=$1
+TEST_COMMAND=$2
+
+if [ -n "$Grpc__CaCert" ]; then
+  export Grpc__CaCert=$working_dir/csharp/$Grpc__CaCert
+fi
+if [ -n "$Grpc__ClientCert" ]; then
+  export Grpc__ClientCert=$working_dir/csharp/$Grpc__ClientCert
+fi
+if [ -n "$Grpc__ClientKey" ]; then
+  export Grpc__ClientKey=$working_dir/csharp/$Grpc__ClientKey
+fi
+
+cd $working_dir/csharp
+set +e
+set -x
+./out/ArmoniK.Api.Mock.exe &
+  server_pid=$!
+  sleep 5
+
+set -e
+
+cd $working_dir
+cd $TEST_DIR
+
+$TEST_COMMAND
+ret=$?
+exit $ret
+set +e
+
+echo $server_pid
+kill $server_pid
