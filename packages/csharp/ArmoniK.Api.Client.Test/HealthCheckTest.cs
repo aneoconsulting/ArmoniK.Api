1c0055489deed94a83c14bcc8ad5a0bedbf12e6c
Daouda Diakite <ddiakite@aneo.fr>
2024-10-29 10:10:11 +0100

    Add health check test

---
+++ packages/csharp/ArmoniK.Api.Client.Test/HealthCheckTest.cs
@@ -0,0 +1,81 @@
+﻿// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2021-2024. All rights reserved.
+//   W. Kirschenmann   <wkirschenmann@aneo.fr>
+//   J. Gurhem         <jgurhem@aneo.fr>
+//   D. Dubuc          <ddubuc@aneo.fr>
+//   L. Ziane Khodja   <lzianekhodja@aneo.fr>
+//   F. Lemaitre       <flemaitre@aneo.fr>
+//   S. Djebbar        <sdjebbar@aneo.fr>
+//   J. Fonseca        <jfonseca@aneo.fr>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+
+using ArmoniK.Api.Client.Options;
+using ArmoniK.Api.Client.Submitter;
+using ArmoniK.Api.gRPC.V1.HealthChecks;
+
+using NUnit.Framework;
+
+namespace ArmoniK.Api.Client.Tests;
+
+[TestFixture]
+public class HealthCheckTest
+{
+  [SetUp]
+  public void SetUp()
+  {
+    certPath_       = Environment.GetEnvironmentVariable("Grpc__ClientCert")               ?? "";
+    keyPath_        = Environment.GetEnvironmentVariable("Grpc__ClientKey")                ?? "";
+    CaCertPath_     = Environment.GetEnvironmentVariable("Grpc__CaCert")                   ?? "";
+    MessageHandler_ = Environment.GetEnvironmentVariable("GrpcClient__HttpMessageHandler") ?? "";
+    endpoint_       = Environment.GetEnvironmentVariable("Grpc__Endpoint")                 ?? "";
+    isInsecure_     = Environment.GetEnvironmentVariable("Grpc__AllowUnsafeConnection") == "true";
+
+    if (isInsecure_)
+    {
+      endpoint_ = RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework") || MessageHandler_.ToLower()
+                                                                                                         .Contains("web")
+                    ? "http://localhost:4999"
+                    : endpoint_;
+    }
+  }
+
+  private static string? endpoint_;
+  private static string? certPath_;
+  private static string? keyPath_;
+  private static string? CaCertPath_;
+  private static string? MessageHandler_;
+  private        bool    isInsecure_;
+
+  [Test]
+  public void TestHealthCheck()
+  {
+    var channel = GrpcChannelFactory.CreateChannel(new GrpcClient
+                                                   {
+                                                     Endpoint              = endpoint_,
+                                                     AllowUnsafeConnection = isInsecure_,
+                                                     CertPem               = certPath_!,
+                                                     KeyPem                = keyPath_!,
+                                                     CaCert                = CaCertPath_!,
+                                                     HttpMessageHandler    = MessageHandler_!,
+                                                   });
+    var client = new HealthChecksService.HealthChecksServiceClient(channel);
+    Assert.That(() => client.CheckHealth(new CheckHealthRequest()),
+                Throws.Nothing);
+  }
+}
