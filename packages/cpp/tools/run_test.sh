2251e886571e32a74ab01904de6ef14125b7d77f
Daouda Diakite <ddiakite@aneo.fr>
2024-11-06 11:55:46 +0100

    Using http handler in ci

--- packages/cpp/tools/run_test.sh
+++ packages/cpp/tools/run_test.sh
@@ -4,11 +4,11 @@ set -x
 script_path="$(dirname "${BASH_SOURCE:-$0}")"
 CertFolder="$(realpath "$script_path/../../csharp/certs" )"
 docker run --rm -t --network host -v "$CertFolder:/app/source/certs" -v "/usr/local/share/ca-certificates/:/usr/local/share/ca-certificates" \
-            -e Grpc__EndPoint="$Grpc__Endpoint" \
+            -e Grpc__EndPoint \
             -e Http__EndPoint="${Http__Endpoint:-$Grpc__Endpoint}" \
-            ${Grpc__SSLValidation:+-e Grpc__SSLValidation="$Grpc__SSLValidation"} \
+            ${Grpc__SSLValidation:+-e Grpc__SSLValidation} \
             ${Grpc__CaCert:+-e Grpc__CaCert="/app/source/certs/server1-ca.pem"} \
-            ${Grpc__mTLS:+-e Grpc__mTLS="$Grpc__mTLS"} \
+            ${Grpc__mTLS:+-e Grpc__mTLS} \
             ${Grpc__ClientCert:+-e Grpc__ClientCert="/app/source/certs/client.pem"} \
             ${Grpc__ClientKey:+-e Grpc__ClientKey="/app/source/certs/client.key"} \
             "armonik-api-cpp:0.1.0"
