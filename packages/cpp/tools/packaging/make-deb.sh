ae6f96c71c8f809b7ef803dde848537882291c99
Nicolas Gruel <ngruel@aneo.fr>
2024-12-09 14:58:54 +0100

    fix: correct binary client creation based on alpine

--- packages/cpp/tools/packaging/make-deb.sh
+++ packages/cpp/tools/packaging/make-deb.sh
@@ -2,5 +2,5 @@
 set -x
 script_path="$(readlink -f "${BASH_SOURCE:-$0}")"
 script_dir="$(dirname "$script_path")"
-docker build -t "debbuild" -f deb.Dockerfile --progress=plain "${script_dir}/../../../.."
+docker buildx build -t "debbuild" -f deb.Dockerfile --progress=plain "${script_dir}/../../../.."
 docker run --rm -v ".:/host" --entrypoint bash "debbuild" -c "cp ./*.deb /host/"
