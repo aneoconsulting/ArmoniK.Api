ed1c4a198f88d7b126a58c5d19fef7fe26eac9f1
Dylan Brasseur <103190315+dbrasseur-aneo@users.noreply.github.com>
2025-03-28 14:08:55 +0100

    docs: Fix read the doc build

--- scripts/generate-csharp-doc.sh
+++ scripts/generate-csharp-doc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-dotnet tool update -g docfx
+dotnet tool install -g docfx
 dotnet build packages/csharp/ArmoniK.Api.sln
 docfx docfx.json
 sed -E -i 's/([#]+) <a id="([^"]+)"><\/a> (.+)/<a id="\2"><\/a>\n\1 \3/g' .docs/content/api/csharp/*.md
