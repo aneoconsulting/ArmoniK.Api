7c26d978466124663f4bbdc1a5b688a4d9030d38
Florian Lemaitre <flemaitre@aneo.fr>
2025-06-05 23:26:04 +0200

    force x509v3

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -28,16 +28,17 @@ stateOrProvinceName    = France
 organizationName       = ArmoniK Ingress Root (NonTrusted)
 organizationalUnitName = $1
 commonName             = ${2:-ArmoniK Ingress Root (NonTrusted) User Certificate}
-${2:+[ v3_req ]}
+[ v3_req ]
+keyUsage = digitalSignature, keyEncipherment, keyAgreement
 ${2:+subjectAltName = @alt_names}
 ${2:+[ alt_names ]}
 ${2:+DNS.1 = $2}
 EOF
 
   openssl genrsa -out "$1.key" 4096
-  openssl req -config "$1.cnf" -new -key "$1".key -out "$1".csr ${2:+-extensions v3_req}
+  openssl req -config "$1.cnf" -new -key "$1".key -out "$1".csr -extensions v3_req
 
-  openssl x509 -req -in "$1.csr" -CA "$1-ca.pem" -CAkey "$1-ca.key" -CAcreateserial -out "$1.pem" -days 3650 -extfile "$1.cnf" ${2:+-extensions v3_req}
+  openssl x509 -req -in "$1.csr" -CA "$1-ca.pem" -CAkey "$1-ca.key" -CAcreateserial -out "$1.pem" -days 3650 -extfile "$1.cnf" -extensions v3_req
   openssl pkcs12 -export -out "$1.p12" -inkey "$1.key" -in "$1.pem" -passout pass:
   openssl pkcs12 -in "$1.p12" -out "$1-client.pem" -nodes -passout pass: -passin pass:""
 }
