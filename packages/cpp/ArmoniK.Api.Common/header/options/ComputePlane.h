44950d5ecf062dd22a77901bd86da950db0c593f
José Fonseca <jfonseca@aneo.fr>
2026-03-20 15:33:28 +0100

    work on review remarks

--- packages/cpp/ArmoniK.Api.Common/header/options/ComputePlane.h
+++ packages/cpp/ArmoniK.Api.Common/header/options/ComputePlane.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <sstream>
 #include <utils/Configuration.h>
+#include "GrpcSocketType.h"
 
 /**
  * @brief The armonik namespace contains classes and functions related to the ArmoniK API.
@@ -20,7 +21,9 @@ public:
    */
   ComputePlane(const utils::Configuration &configuration) {
     set_worker_address(configuration.get("ComputePlane__WorkerChannel__Address"));
+    set_worker_socket_type(configuration.get("ComputePlane__WorkerChannel__SocketType"));
     set_agent_address(configuration.get(std::string("ComputePlane__AgentChannel__Address")));
+    set_agent_socket_type(configuration.get("ComputePlane__AgentChannel__SocketType"));
   }
 
   /**
@@ -29,47 +32,85 @@ public:
    */
   [[nodiscard]] const std::string &get_server_address() const { return worker_address_; }
 
+  /**
+   * @brief Returns the server socket type.
+   * @return The server socket type as a grpc_socket_type enum value.
+   */
+  [[nodiscard]] grpc_socket_type get_server_socket_type() const { return worker_socket_type_; }
+
   static bool starts_with(absl::string_view s, absl::string_view prefix) {
     return s.size() >= prefix.size() && s.substr(0, prefix.size()) == prefix;
   }
 
   /**
    * @brief Strips http/https schemes and ensures a valid gRPC address format.
-   * @param address The address to normalize.
+   * @param address The address to normalize, provided as an absl::string_view.
    * @return The normalized address.
    */
-  static std::string normalize_address(const std::string &address) {
-    absl::string_view av(address);
-    if (starts_with(av, "https://")) {
-      return std::string(av.substr(8).begin(), av.substr(8).end());
+  static std::string normalize_address(absl::string_view address) {
+    if (starts_with(address, "https://")) {
+      return std::string(address.substr(8).begin(), address.substr(8).end());
     }
-    if (starts_with(av, "http://")) {
-      return std::string(av.substr(7).begin(), av.substr(7).end());
+    if (starts_with(address, "http://")) {
+      return std::string(address.substr(7).begin(), address.substr(7).end());
     }
     // No recognized leading scheme: assume unix socket"
-    return std::string("unix://") + address;
+    return std::string("unix://") + std::string(address);
   }
 
   /**
    * @brief Sets the worker address with the given socket address.
-   * @param socket_address The socket address to set for the worker.
+   * @param worker_address The socket address to set for the worker.
    */
-  void set_worker_address(const std::string socket_address) { worker_address_ = normalize_address(socket_address); }
+  void set_worker_address(absl::string_view worker_address) { worker_address_ = normalize_address(worker_address); }
+
+  /**
+   * @brief Sets the worker socket type
+   * @param socket_type The socket type string from configuration.
+   */
+  void set_worker_socket_type(absl::string_view socket_type) {
+    if (starts_with(socket_type, "tcp")){
+      worker_socket_type_ = grpc_socket_type::tcp;
+    }else{
+      worker_socket_type_ = grpc_socket_type::UnixDomainSocket;
+    }
+  }
 
   /**
    * @brief Sets the agent address with the given agent address.
    * @param agent_address The agent address to set for the agent.
    */
-  void set_agent_address(const std::string agent_address) { agent_address_ = normalize_address(agent_address); }
+  void set_agent_address(absl::string_view agent_address) { agent_address_ = normalize_address(agent_address); }
+
+    /**
+   * @brief Sets the worker socket type
+   * @param socket_address The socket type string from configuration.
+   */
+  void set_agent_socket_type(absl::string_view socket_type) {
+    if (starts_with(socket_type, "tcp")){
+      agent_socket_type_ = grpc_socket_type::tcp;
+    }else{
+      agent_socket_type_ = grpc_socket_type::UnixDomainSocket;
+    }
+  }
+
   /**
    * @brief Returns the agent address.
    * @return A reference to the agent address string.
    */
   [[nodiscard]] const std::string &get_agent_address() const { return agent_address_; }
 
+  /**
+   * @brief Returns the agent socket type.
+   * @return The agent socket type as a grpc_socket_type enum value.
+   */
+  [[nodiscard]] grpc_socket_type get_agent_socket_type() const { return agent_socket_type_; }
+
 private:
   std::string worker_address_; ///< The worker address string.
   std::string agent_address_;  ///< The agent address string.
+  grpc_socket_type worker_socket_type_;
+  grpc_socket_type agent_socket_type_;
 };
 } // namespace options
 } // namespace common
