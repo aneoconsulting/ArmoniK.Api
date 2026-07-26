494edbb274f4f39e970f39fd3b171dd2766cf1c0
Florian Lemaitre <flemaitre@aneo.fr>
2026-07-26 18:56:37 +0200

    chore(rust): drop the dead health-checks conversions to the generated enum
    Leftover From impls between health_checks::Status and the generated
    HealthStatusEnum that the conversion-layer deletion missed. Nothing
    references them.

--- packages/rust/armonik/src/objects/health_checks/status.rs
+++ packages/rust/armonik/src/objects/health_checks/status.rs
@@ -1,5 +1,3 @@
-use crate::api::v3;
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, armonik_macros::Enum)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[armonik(enum = "armonik.api.grpc.v1.health_checks.HealthStatusEnum")]
@@ -13,16 +11,4 @@ pub enum Status {
     /// Unspecified (zero) or a status unknown to this crate version;
     /// round-trips losslessly.
     Other(OtherStatus),
-}
-
-impl From<Status> for v3::health_checks::HealthStatusEnum {
-    fn from(value: Status) -> Self {
-        Self::try_from(i32::from(value)).unwrap_or(Self::Unspecified)
-    }
-}
-
-impl From<v3::health_checks::HealthStatusEnum> for Status {
-    fn from(value: v3::health_checks::HealthStatusEnum) -> Self {
-        Self::from(value as i32)
-    }
 }
